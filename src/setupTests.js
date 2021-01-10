a5f56140fd45eeef2ef82baf8c18e159cebd3699
Warren C. Longmire <warren.longmire@lambdaschool.com>
2021-01-10 17:22:47 -0500

    removed mutator.

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,3 @@
-import MutationObserver from '@sheerun/mutationobserver-shim';
-window.MutationObserver = MutationObserver;
-
 import '@testing-library/jest-dom/extend-expect';
 
 import { server } from './mocks/server.js';
